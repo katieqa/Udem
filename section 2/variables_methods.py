2d37c04d3ce1352e5e5ee90bac039a4375491cb0
K <kabosma@gmail.com>
2018-06-23 09:59:55 -0700

    Section 2 code part 1

---
+++ section 2/variables_methods.py
@@ -0,0 +1,32 @@
+a = 5
+b = 6
+my_variable = 56
+
+string_variable = "hello"
+string_variable2 = 'hello'
+
+var1 = "scotsmen"
+var2 = var1
+
+num1 = 4
+num2 = 4
+
+print(var2)
+print(num1*num2)
+
+##
+
+def my_print_method(input):
+    print("hello")
+    print(input)
+
+my_print_method(var2)
+
+def my_multiply_method(val1, val2):
+    return val1*val2
+
+my_print_method(my_multiply_method(num1,num2))
+
+##
+
+
